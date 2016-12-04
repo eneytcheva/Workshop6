de216b39aa1948b928bda7c9bf596cee41d1b755
Elitza Neytcheva <eneytcheva@gmail.com>
2016-12-03 21:08:07 -0500

    almostDone

--- server/src/server.js
+++ server/src/server.js
@@ -18,21 +18,21 @@ app.use(bodyParser.json());
 app.use(express.static('../client/build'));
 
 /**
-* Get the feed data for a particular user.
-*/
+ * Get the feed data for a particular user.
+ */
 app.get('/user/:userid/feed', function(req, res) {
-var userid = req.params.userid;
-var fromUser = getUserIdFromToken(req.get('Authorization'));
-// userid is a string. We need it to be a number.
-// Parameters are always strings.
-var useridNumber = parseInt(userid, 10);
-if (fromUser === useridNumber) {
-// Send response.
-res.send(getFeedData(userid));
-} else {
-// 401: Unauthorized request.
-res.status(401).end();
-}
+    var userid = req.params.userid;
+    var fromUser = getUserIdFromToken(req.get('Authorization'));
+    // userid is a string. We need it to be a number.
+    // Parameters are always strings.
+    var useridNumber = parseInt(userid, 10);
+    if (fromUser === useridNumber) {
+        // Send response.
+        res.send(getFeedData(userid));
+    } else {
+        // 401: Unauthorized request.
+        res.status(401).end();
+    }
 });
 
 
@@ -42,7 +42,9 @@ app.listen(3000, function() {
 });
 
 // `POST /feeditem { userId: user, location: location, contents: contents }`
-app.post('/feeditem', validate({body: StatusUpdateSchema}), function(req, res) {
+app.post('/feeditem', validate({
+    body: StatusUpdateSchema
+}), function(req, res) {
     // If this function runs, `req.body` passed JSON validation!
     var body = req.body;
     var fromUser = getUserIdFromToken(req.get('Authorization'));
@@ -61,6 +63,151 @@ app.post('/feeditem', validate({body: StatusUpdateSchema}), function(req, res) {
         res.status(401).end();
     }
 });
+
+// Reset database.
+app.post('/resetdb', function(req, res) {
+    console.log("Resetting database...");
+    // This is a debug route, so don't do any validation.
+    database.resetDatabase();
+    // res.send() sends an empty response with status code 200
+    res.send();
+});
+
+// Update a feed item.
+app.put('/feeditem/:feeditemid/content', function(req, res) {
+    var fromUser = getUserIdFromToken(req.get('Authorization'));
+    var feedItemId = req.params.feeditemid;
+    var feedItem = readDocument('feedItems', feedItemId);
+    // Check that the requester is the author of this feed item.
+    if (fromUser === feedItem.contents.author) {
+        // Check that the body is a string, and not something like a JSON object.
+        // We can't use JSON validation here, since the body is simply text!
+        if (typeof(req.body) !== 'string') {
+            // 400: Bad request.
+            res.status(400).end();
+            return;
+        }
+        // Update text content of update.
+        feedItem.contents.contents = req.body;
+        writeDocument('feedItems', feedItem);
+        res.send(getFeedItemSync(feedItemId));
+    } else {
+        // 401: Unauthorized.
+        res.status(401).end();
+    }
+});
+
+/**
+ * Delete a feed item.
+ */
+app.delete('/feeditem/:feeditemid', function(req, res) {
+    var fromUser = getUserIdFromToken(req.get('Authorization'));
+    // Convert from a string into a number.
+    var feedItemId = parseInt(req.params.feeditemid, 10);
+    var feedItem = readDocument('feedItems', feedItemId);
+    // Check that the author of the post is requesting the delete.
+    if (feedItem.contents.author === fromUser) {
+        database.deleteDocument('feedItems', feedItemId);
+        // Remove references to this feed item from all other feeds.
+        var feeds = database.getCollection('feeds');
+        var feedIds = Object.keys(feeds);
+        feedIds.forEach((feedId) => {
+            var feed = feeds[feedId];
+            var itemIdx = feed.contents.indexOf(feedItemId);
+            if (itemIdx !== -1) {
+                // Splice out of array.
+                feed.contents.splice(itemIdx, 1);
+                // Update feed.
+                database.writeDocument('feeds', feed);
+            }
+        });
+        // Send a blank response to indicate success.
+        res.send();
+    } else {
+        // 401: Unauthorized.
+        res.status(401).end();
+    }
+});
+
+// Like a feed item.
+app.put('/feeditem/:feeditemid/likelist/:userid', function(req, res) {
+    var fromUser = getUserIdFromToken(req.get('Authorization'));
+    // Convert params from string to number.
+    var feedItemId = parseInt(req.params.feeditemid, 10);
+    48
+    var userId = parseInt(req.params.userid, 10);
+    if (fromUser === userId) {
+        var feedItem = readDocument('feedItems', feedItemId);
+        // Add to likeCounter if not already present.
+        if (feedItem.likeCounter.indexOf(userId) === -1) {
+            feedItem.likeCounter.push(userId);
+            writeDocument('feedItems', feedItem);
+        }
+        // Return a resolved version of the likeCounter
+        res.send(feedItem.likeCounter.map((userId) =>
+            readDocument('users', userId)));
+    } else {
+        // 401: Unauthorized.
+        res.status(401).end();
+    }
+});
+
+// Unlike a feed item.
+app.delete('/feeditem/:feeditemid/likelist/:userid', function(req, res) {
+    var fromUser = getUserIdFromToken(req.get('Authorization'));
+    // Convert params from string to number.
+    var feedItemId = parseInt(req.params.feeditemid, 10);
+    var userId = parseInt(req.params.userid, 10);
+    if (fromUser === userId) {
+        var feedItem = readDocument('feedItems', feedItemId);
+        var likeIndex = feedItem.likeCounter.indexOf(userId);
+        // Remove from likeCounter if present
+        if (likeIndex !== -1) {
+            feedItem.likeCounter.splice(likeIndex, 1);
+            writeDocument('feedItems', feedItem);
+        }
+        // Return a resolved version of the likeCounter
+        // Note that this request succeeds even if the
+        // user already unliked the request!
+        res.send(feedItem.likeCounter.map((userId) =>
+            readDocument('users', userId)));
+    } else {
+        // 401: Unauthorized.
+        res.status(401).end();
+    }
+});
+
+// Search for feed item
+app.post('/search', function(req, res) {
+    var fromUser = getUserIdFromToken(req.get('Authorization'));
+    var user = readDocument('users', fromUser);
+    if (typeof(req.body) === 'string') {
+        // trim() removes whitespace before and after the query.
+        // toLowerCase() makes the query lowercase.
+        var queryText = req.body.trim().toLowerCase();
+        // Search the user's feed.
+        var feedItemIDs = readDocument('feeds', user.feed).contents;
+        // "filter" is like "map" in that it is a magic method for
+        // arrays. It takes an anonymous function, which it calls
+        // with each item in the array. If that function returns 'true',
+        // it will include the item in a return array. Otherwise, it will
+        // not.
+        // Here, we use filter to return only feedItems that contain the
+        // query text.
+        // Since the array contains feed item IDs, we later map the filtered
+        // IDs to actual feed item objects.
+        res.send(feedItemIDs.filter((feedItemID) => {
+            var feedItem = readDocument('feedItems', feedItemID);
+            return feedItem.contents.contents
+                .toLowerCase()
+                .indexOf(queryText) !== -1;
+        }).map(getFeedItemSync));
+    } else {
+        // 400: Bad Request.
+        res.status(400).end();
+    }
+});
+
 
 /**
  * Resolves a feed item. Internal to the server, since it's synchronous.
@@ -117,8 +264,8 @@ function getUserIdFromToken(authorizationLine) {
 }
 
 /**
-* Adds a new status update to the database.
-*/
+ * Adds a new status update to the database.
+ */
 function postStatusUpdate(user, location, contents) {
     // If we were implementing this for real on an actual server, we would check
     // that the user ID is correct & matches the authenticated user. But since
@@ -153,7 +300,9 @@ function postStatusUpdate(user, location, contents) {
 }
 
 // `POST /feeditem { userId: user, location: location, contents: contents }`
-app.post('/feeditem', validate({body: StatusUpdateSchema}), function(req, res) {
+app.post('/feeditem', validate({
+    body: StatusUpdateSchema
+}), function(req, res) {
     // If this function runs, `req.body` passed JSON validation!
     var body = req.body;
     var fromUser = getUserIdFromToken(req.get('Authorization'));
